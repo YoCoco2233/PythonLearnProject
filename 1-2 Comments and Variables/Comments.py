0441d3c2998daf74fef63c50d1acf5a7207a7f45
YoCoco <YoCoco2233@outlook.com>
2024-06-19 01:10:00 +0800

    1-2-1 Comments 2024-6-19 0:35 start,1:08 end  @YoCoco2233

---
+++ 1-2 Comments and Variables/Comments.py
@@ -0,0 +1,41 @@
+# 2024-6-19 0:35 start,1:08 end  @YoCoco2233 Comments
+# Du kan om du vil, Ingen kan ta ditt valg (种下理想便能成功,无人可挡)
+
+# This is a comment explaining the purpose of the code
+def my_function():
+    # Another comment inside the function
+    pass
+
+# ''' ''' == """ """
+"""
+This is a mult-line comment example.
+It is often used to provide detailed explanations or descriptions
+for sections of code ,especially when those ezplanations span multiple lines.
+"""
+def calculate_sum(a,b):
+    '''
+
+    :param a:
+    :param b:
+    :return:
+    '''
+    return a + b
+"""
+This function calculates the sum of two numbers
+args:
+a(int):the first numberto add.
+b(int):the second numberto add.
+returns: The sum of the two numbers.
+    
+"""
+"""
+While loop
+for loop
+"""
+if __name__ == '__main__':
+    output= calculate_sum(22,33)
+    print(output)
+
+
+
+
