01985f5adcaae89ef856bebd4ee0e0143ecf644c
YoCoco <YoCoco2233@outlook.com>
2024-06-19 23:55:17 +0800

    # 2024-6-19 14:30 start,23:55 end  @YoCoco2233 Variables

---
+++ 1-2 Comments and Variables/Variables.py
@@ -0,0 +1,37 @@
+# 2024-6-19 14:30 start,23:55 end  @YoCoco2233 Variables
+"""
+    Mange mål som kan nåes om du våger, å tro på at du har det som skal til
+    (敢于做便能达成目标,相信自己有这个能力)
+"""
+# VarName = Var
+# define variable
+name = '元可可'    # String
+age = 19          # int
+
+# referencing the variable
+print(name)
+print(age)
+# print multiple variables together
+print(name,age)
+
+"""
+MyName
+myName
+_myName
+my_name
+
+:Case
+Uppercase:A,B,C,D...
+Lowercase:a,b,c,d...
+
+:keyword
+False: continue,from,not,yield
+None: def,global,or
+True: del,if,pass
+and: elif,import,raise
+as: else,in,return
+assert: expect,is,try
+break: finally,lambda,while
+class: for,nonlocal,with
+"""
+
