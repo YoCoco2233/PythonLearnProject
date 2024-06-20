695d0e13a6a5e4ed1ce74c1cc9c82d14441680f2
YoCoco <YoCoco2233@outlook.com>
2024-06-20 14:43:04 +0800

    # 2024-6-20 14:00 start, 2024-6-20 14:45 end  @YoCoco2233 Format

---
+++ 1-4 Formatted Output/1-4-1 Print.py
@@ -0,0 +1,19 @@
+# 2024-6-20 14:10 start, 2024-6-20 14:20 end  @YoCoco2233 Print()
+"""
+Det er så mange hensyn å ta, Mange strenger som drar hver sin retning
+(过分考虑,只会适得其反)
+"""
+name = '元可可'
+age = 19
+
+print(name)
+print(age)
+print(name,age)
+
+"""
+print(value, end = '\n')
+"""
+print(name, end='')
+print(age, end='')
+
+print(name, end= '\t')
---
+++ 1-4 Formatted Output/1-4-2 Percent Sign.py
@@ -0,0 +1,34 @@
+# 2024-6-20 14:20 start, 2024-6-20 14:42 end  @YoCoco2233 Percent Sign
+"""
+Frykten for feiltrinn kan mørklegge alt
+(害怕犯错只会使我们离成功越来越远)
+"""
+_ValueName = 'Value'
+print(_ValueName)
+
+"""
+print('String % Format' % (_ValueName))
+print('String % Format % Format' % (ValueName0,ValueName1, ValueNameN))
+
+%s 字符串
+%d 有浮点的十进制整数
+%f 浮点数
+%c 字符
+%u 无符号十进制整数
+%o 八进制整数  
+%x 十六进制整数（小写ox)
+%X 十六进制整数（大写OX）
+%e 科学计数法（小写‘e')
+%E 科学计数法（大写'E')
+%g %f和%e的简写
+%G %F和%E的简写
+
+"""
+name = '元可可'
+age = 19
+
+print('My name is 元可可, and I am 19 years old')
+# Python2/3
+print('My name is %s, and I am %d years old' % (name, age))
+
+
