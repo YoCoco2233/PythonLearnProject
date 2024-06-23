d486e061a28b5f8352905bcfc4316308a041efe2
yococo <YoCoco2233@outlook.com>
2024-06-23 18:38:32 +0800

    # 2024-6-23 18:22 start, 2024-6-23 18:35 end  @YoCoco2233 Swapping two values

---
+++ 1-5 Calculate/1-5-2 Swapping two values.py
@@ -0,0 +1,18 @@
+# 2024-6-23 18:22 start, 2024-6-23 18:35 end  @YoCoco2233 Swapping two values
+"""
+Swap the values of variables A and B
+1,temporary variable
+2,tuple unpacking
+"""
+
+# 1 Using a temporary variable
+a = 5
+b = 10
+temp = a  # Store the value of a in a temporary variable
+a = b  # Assign the value of b to a
+b = temp  # Assign the temporary value(originally A's value to b)
+
+# 2 Using tuple unpacking
+_a = 5
+_b = 10
+_a, _b = _b, _a  # Swap the value of a and b using tuple unpacking
