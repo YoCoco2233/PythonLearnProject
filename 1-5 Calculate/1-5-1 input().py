6b5556b7be2c1f05991644b7f3ef6abcb6a0a245
yococo <YoCoco2233@outlook.com>
2024-06-23 18:12:47 +0800

    # 2024-6-23 14:40 start, 2024-6-23 14:55 end  @YoCoco2233 input()

---
+++ 1-5 Calculate/1-5-1 input().py
@@ -0,0 +1,17 @@
+# 2024-6-23 14:40 start, 2024-6-23 14:55 end  @YoCoco2233 input()
+"""
+input()
+"""
+input('Please input your Name: ')
+# Save input in variables
+_name = input('Input your Name:')
+print('This is your name: '+_name)
+
+
+_username = input('Input your Name:')
+_password = input('Input Your Password: ')
+
+if _username == 'YoCoco' and _password == '2233':
+    print('Welcome '+ _username)
+else:
+    print('Input Error,Please try again')
