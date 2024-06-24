868abdb4027c6d1bc9ced3cb0ce269f89dc9d3ae
YoCoco <YoCoco2233@outlook.com>
2024-06-24 15:13:55 +0800

    # 2024-6-22 14:10 start, 2024-6-22 14:31 end  @YoCoco2233 Format the output variables

---
+++ 1-4 Formatted Output/1-4-4 Format method.py
@@ -0,0 +1,13 @@
+# 2024-6-22 14:32 start, 2024-6-22 14: end  @YoCoco2233 Format method
+
+_Varname1 = '2233'
+print('String{}'.format(_Varname1))
+
+_name = '元可可'
+_age = 19
+print('Name:{},Age:{}'.format(_name, _age))
+
+# After Python 3.6
+_name = '元可可'
+_age = 19
+print(f'Name:{_name},Age:{_age}')
