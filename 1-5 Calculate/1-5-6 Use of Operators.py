cbd3c34dfd72893f9ac490df516a361f441a6455
YoCoco <YoCoco2233@outlook.com>
2024-06-28 23:52:28 +0800

    # 2024-6-28 15:27 start, 2024-6-28 23:55 end  @YoCoco2233 Use of Operators

---
+++ 1-5 Calculate/1-5-6 Use of Operators.py
@@ -0,0 +1,25 @@
+# 2024-6-28 15:27 start, 2024-6-28 23:52 end  @YoCoco2233 Use of Operators
+"""
+Addition Operator (+): Described as adding, with an example of 1+1 resulting in 2.
+Subtraction Operator (-): Described as subtracting, with an example of 1-1 resulting in 0.
+Multiplication Operator (): Described as multiplying, with an example of 22 resulting in 4.
+Bit Shift Operator (Shift +8): Typically used in binary operations, but the specific example is not given here.
+Division Operator (/): Described as dividing, but the example of 10/2 resulting in 5 is inaccurate. The actual result should be 5 with a remainder of 0, or simply 5 (depending on the context whether the remainder is considered).
+Integer Division Operator (Left Slash): Described as performing integer division, taking only the quotient, with an example of 9/4 resulting in 2.
+Modulus Operator (%): Described as taking the remainder, with an example of 9%4 resulting in 2.
+Exponentiation Operator (): Described as raising to a power, with an example of 24 resulting in 16, which is 2 multiplied by itself 3 times.
+Parentheses: Described as enhancing the order of operations, with an example of (1+2)*3 resulting 9
+"""
+
+num1 = 22
+num2 = 33
+num3 = 1
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** 2)
+print((num1 + num2) * num3)
