ab47a84a1fede7d90aff9e6f8ecf590d5d950f10
YoCoco <YoCoco2233@outlook.com>
2024-06-29 23:58:25 +0800

    # 2024-6-29 23:55 start, 2024-6-29 23:58 end  @YoCoco2233 Assignment Operater

---
+++ 1-5 Calculate/1-5-7 Assignment Operater.py
@@ -0,0 +1,12 @@
+# 2024-6-29 23:55 start, 2024-6-29 23:58 end  @YoCoco2233 Assignment Operater
+num = 2233
+print(num)
+
+num1, str1, bool1 = 2233, 'YoCoco2233', True
+print(num1)
+print(str1)
+print(bool1)
+
+a = b = 2233
+print(a)
+print(b)
