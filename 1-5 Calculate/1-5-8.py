aa2c67f54e0e2bb7887e4e983afb30546b578cea
YoCoco <YoCoco2233@outlook.com>
2024-06-30 23:47:22 +0800

    # 2024-6-30 23:20 start, 2024-6-30 23:45 end  @YoCoco2233 Compound Assignment Operator

---
+++ 1-5 Calculate/1-5-8.py
@@ -0,0 +1,70 @@
+# 2024-6-30 23:20 start, 2024-6-30 23:45 end  @YoCoco2233 Compound Assignment Operator
+# Addition Assignment Operator
+a = 22
+a += 33
+print(a)
+a = a + 33
+print(a)
+
+# Subtraction Assignment Operator
+b = 2
+b -= 1
+print(b)
+b = b -1
+print(b)
+
+# Multiplication Assignment Operator
+c = 22
+c *= 2
+print(c)
+c = c * 2
+print(c)
+
+# Division Assignment Operator
+d = 22
+d /= 2
+print(d)
+d = d / 2
+print(d)
+
+# Modulus Assignment Operator
+e = 9
+e %= 7
+print(e)
+e = e % 7
+print(e)
+
+# Bitwise AND Assignment Operator
+f = 0x11
+f &= 0x15
+print(f)
+f = f & 0x15
+print(f)
+
+# Bitwise OR Assignment Operator
+g = 0x11
+g |= 0x0A
+print(g)
+g = g | 0xAA
+print(g)
+
+# Bitwise XOR Assignment Operator
+h = 0x11
+h ^= 0xFF
+print(h)
+h = h ^ 0xFF
+print(h)
+
+# Lift Shift Assignment Operator
+i = 3
+i <<= 2
+print(i)
+i = i << 2
+print(i)
+
+# Right Shift Assignment Operator
+j = 9
+j >>= 1
+print(j)
+j = j >> 1
+print(j)
