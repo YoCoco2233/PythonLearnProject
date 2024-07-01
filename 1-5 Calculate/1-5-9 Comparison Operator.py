80f6850d553d4f1dd5b1d293ef970af78f8b7070
YoCoco <YoCoco2233@outlook.com>
2024-07-01 23:44:41 +0800

    # 2024-7-1 23:38 start, 2024-7-1 23:45 end  @YoCoco2233 Comparison Operator

---
+++ 1-5 Calculate/1-5-9 Comparison Operator.py
@@ -0,0 +1,9 @@
+# 2024-7-1 23:38 start, 2024-7-1 23:45 end  @YoCoco2233 Comparison Operator
+num1 = 22
+num2 = 33
+print(f'Number1 > Number2: {num1 > num2}')
+print(f'Number1 < Number2: {num1 < num2}')
+print(f'Number1 >= Number2: {num1 >= num2}')
+print(f'Number1 <= Number2: {num1 <= num2}')
+print(f'Number1 == Number2: {num1 == num2}')
+print(f'Number1 !=0 Number2: {num1 != num2}')
