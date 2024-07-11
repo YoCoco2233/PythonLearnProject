96457f7d470debd4fa87ad36a91b8f939e99a619
YoCoco2233 <YoCoco2233@outlook.com>
2024-07-11 16:34:57 +0800

    # 2024-7-11 16:10 start, 2024-7-11 16:34 end  @YoCoco2233 If Statement

---
+++ 1-6 Branch Statement/1-6-1 If Statement.py
@@ -0,0 +1,15 @@
+# 2024-7-11 16:10 start, 2024-7-11 16:34 end  @YoCoco2233 If Statement
+# define age
+age = 19
+if age >= 18:
+    print("You are a teenage.")
+else:
+    print("You are a kid")
+# with  input(),need to type conversion..
+age2 = int(input("Enter your age: "))
+print(type(age2))
+# >= not  comparison int and String
+if age2 >= 18:
+    print("You are a teenage.")
+else:
+    print("You are a kid")
