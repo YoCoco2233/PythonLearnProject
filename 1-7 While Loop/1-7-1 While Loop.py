980e0a0d29f5bf926d4f5abdc2ca0e4d40f08ccf
YoCoco2233 <YoCoco2233@outlook.com>
2024-07-15 21:47:52 +0800

    # 2024-7-15 21:41 start, 2024-7-15 21:57 end  @YoCocoium While Loop

---
+++ 1-7 While Loop/1-7-1 While Loop.py
@@ -0,0 +1,12 @@
+# 2024-7-15 21:41 start, 2024-7-15 21:47 end  @YoCocoium While Loop
+# Initialize a counter variable.
+
+counter = 0  # Set the counter to 0
+
+# Set a condition for the while loop
+while counter < 22:  # The loop while continue until counter reaches 10
+    print(counter)  # print the current value of the counter
+    counter += 1
+
+# After the loop, print a final message
+print("Loop Finished.")
