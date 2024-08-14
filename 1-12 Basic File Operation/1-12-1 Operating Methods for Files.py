fdb5a9961b3b97c63c3bbf6e530aeb37686bf72b
YoCoco <YoCoco2233@outlook.com>
2024-08-14 22:39:15 +0800

    # 2024-8-13 19:01 start, 2024-8-13 19:46 end  @YoCoco2233 1-12-1 Operating Methods for Files

--- 1-12 Basic File Operation/1-12-1 Operating Methods for Files.py
+++ 1-12 Basic File Operation/1-12-1 Operating Methods for Files.py
@@ -1,34 +1,33 @@
 # 2024-8-13 19:01 start, 2024-8-13 19:46 end  @YoCoco2233 1-12-1 Operating Methods for Files
 
-# 定义文件路径
+# Define file path
 file_path = 'example.txt'
 
-# 打开文件读取内容，如果文件不存在则报错
-# 使用‘with’语句可以自动管理文件的打开和关闭
-with open(file_path, 'r') as file:  # 'r'表示读取模式
-    # 读取文件全部内容
+# Open the file to read content, if the file does not exist, an error will be raised
+# Using the 'with' statement can automatically manage the opening and closing of the file
+with open(file_path, 'r') as file:  # 'r' represents read mode
+    # Read the entire content of the file
     content = file.read()
-    print("文件内容：")
+    print("File content:")
     print(content)
 
-# 打开文件以写入内容，如果文件不存在则创建
-# 注意：这会覆盖文件原有内容
-with open(file_path, 'w') as file:  # 'w'表示写入模式
-    # 写入内容到文件
+# Open the file to write content, if the file does not exist, it will be created
+# Note: This will overwrite the existing content of the file
+with open(file_path, 'w') as file:  # 'w' represents write mode
+    # Write content to the file
     file.write("Hello YoCoco, This is a new content:\n")
     file.write("Writing to files is easy in Python")
 
-# 打开文件以追加内容
-# 如果文件不存在，则创建文件
-with open(file_path, 'a') as file:  # 'a'表示追加模式
-    # 追加内容到文件
-    file.write("\n加入更多内容：")
+# Open the file to append content
+# If the file does not exist, it will be created
+with open(file_path, 'a') as file:  # 'a' represents append mode
+    # Append content to the file
+    file.write("\nAdding more content:")
 
-# 注意,在实际使用中,'with'语句确保了文件在操作完成后会被正常关闭。
-# 无需显式调用file.close()
+# Note, in practical use, the 'with' statement ensures that the file will be properly closed after the operation.
+# There is no need to explicitly call file.close()
 
-# 如果需要读取文件的每一行，可以使用for循环
+# If you need to read each line of the file, you can use a for loop
 with open(file_path, 'r') as file:
-    for line in file:  # 逐行读取
-        print(line, end='')  # 打印每行，end=''避免打印额外的换行符
-
+    for line in file:  # Read line by line
+        print(line, end='')  # Print each line, end='' avoids printing an additional newline character
