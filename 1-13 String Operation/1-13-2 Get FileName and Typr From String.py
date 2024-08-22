b70ffa365b7b6d1573027757bf472690d2f146d6
YoCoco <YoCoco2233@outlook.com>
2024-08-22 22:04:16 +0800

    # 2024-8-20 19:24 start, 2024-8-20 19:54 end  @YoCoco2233 1-13-2 Get File Name and Type From String

--- 1-13 String Operation/1-13-2 Get FileName and Typr From String.py
+++ 1-13 String Operation/1-13-2 Get FileName and Typr From String.py
@@ -1,20 +1,15 @@
-# 2024-8-20 19:24 start, 2024-8-20 19:54 end  @YoCoco2233 1-13-2 Get File Name and Type From String
+#2024-8-20 19:24 start, 2024-8-20 19:54 end @YoCoco2233 1-13-2 Get File Name and Type From String
+#Upload a file and get the filename and file type using Python
+filename = input('Please enter the file name: ')
 
-
-# 上传文件，用Python获取文件名与文件类型
-
-filename = input('请输入文件名称：')
-
-
-# “.”点号左边的的文件名，点号右边的是文件类型。
+#The part before the last '.' is the filename, and the part after it is the file extension (type).
 index = filename.rfind('.')
-print('文件名字是：' + filename[:index])# 这里index是从左往右数最后一个"."的位置，这里的意思是使用字符串的切片输出这个点号"."左侧的文件名称
-print('文件类型是：' + filename[index:])# 这里index是从左往右数最后一个"."的位置，这里的意思是使用字符串的切片输出这个点号"."右侧侧的文件类型
-#判断文件名称是否包含点号"."
-if filename.rfind(".") != -1: # r+fid() = right find 从右边开始查询，考虑有多个"."的文件如123.tar.gz
+print('The file name is: ' + filename[:index]) # Here, index is the position of the last '.' from the left, slicing the string to get the filename before it.
+print('The file type is: ' + filename[index:]) # Here, index is the position of the last '.' from the left, slicing the string to get the file extension after it.
 
-    print('这是合理的文件')
+#Check if the file name contains a '.'
+if filename.rfind(".") != -1: # rfind() searches from the right, useful for files with multiple '.' like '123.tar.gz'
+
+    print('This is a valid file.')
 else:
-    print('不是合理的文件')
-
-
+    print('This is not a valid file.')
